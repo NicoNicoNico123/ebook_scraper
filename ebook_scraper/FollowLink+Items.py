9932377b0cb3cf8f74858f9cf312a8b45ed9afe8
Nick <nick@inspirationhm.com>
2022-11-29 17:06:07 +0800

    Mongo DataBase

--- ebook_scraper/FollowLink+Items.py
+++ ebook_scraper/FollowLink+Items.py
@@ -1,24 +1,22 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 from scrapy import Item, Field
 from itemloaders.processors import MapCompose, TakeFirst
+
 
 def get_price(txt):
     return float(txt.replace('£', '' ))
 
 def get_quantity(txt):
+    # takes txt as '(19 available)' pattern
+    # and returns 19 from the pattern
     return int(
-        txt.replace('(', "").split()[0]
+        txt.replace('(', '').split()[0]
     )
 
 class EbookItem(Item):
-    title=Field(
+    title = Field(
         output_processor=TakeFirst()
     )
-    price=Field(
+    price = Field(
         input_processor=MapCompose(get_price),
         output_processor=TakeFirst()
     )
--- ebook_scraper/pipelines.py
+++ ebook_scraper/pipelines.py
@@ -1,13 +1,19 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+from pymongo import MongoClient
 from itemadapter import ItemAdapter
-
-
 class EbookScraperPipeline:
+
+    def open_spider(self, spider):
+        self.client = MongoClient(
+            host="mongodb+srv://Nick:Fyzmy1ME1Z3T4kiL@ebookscraper.mgcekl1.mongodb.net/?retryWrites=true&w=majority",
+            connect=False
+        )
+        self.collection = self.client.get_database("ebook").get_collection("travel")
+
     def process_item(self, item, spider):
+        self.collection.insert_one(
+            ItemAdapter(item).asdict()
+        )
         return item
+
+    def close_spider(self, spider):
+        self.client.close()
