56e321845478be776e45590fe0a407834474fb07
Nick <nick@inspirationhm.com>
2022-12-01 15:53:51 +0800

    crsf hacking

--- ebook_scraper/spiders/ebook.py
+++ ebook_scraper/spiders/ebook.py
@@ -1,33 +1,39 @@
 import scrapy
-from ebook_scraper.items import EbookItem
-from scrapy.loader import ItemLoader
+# from ebook_scraper.items import EbookItem
+# from scrapy.loader import ItemLoader
 
 class EbookSpider(scrapy.Spider):
     name="ebook"
-    start_urls = [ "https://books.toscrape.com/catalogue/category/books/travel_2/" ]
+    start_urls = [ "https://www.scrapethissite.com/pages/advanced/?gotcha=csrf" ]
 
     def parse(self, response):
-        ebooks= response.css("article.product_pod")
-
-        for ebook in ebooks:
-            url=ebook.css("h3 a").attrib["href"]
-
-            yield scrapy.Request(
-                url = self.start_urls[0] + url,
-                callback = self.parse_details
-            )
-    def parse_details(self, response):
-        main = response.css('div.product_main')
-
-        loader = ItemLoader(item=EbookItem(), selector=main)
-
-        loader.add_css("title", "h1::text")
-        loader.add_css("price", "p.price_color::text")
-
-        quantity_p = main.css("p.availability")
-        loader.add_value(
-            "quantity",
-            quantity_p.re(r'\(.+ available\)')[0]
+        csrf_token = response.css("input[name='csrf']").attrib["value"]
+        print(
+            "[CSRF]:",
+           csrf_token
         )
-        
-        yield loader.load_item()
+
+        yield scrapy.FormRequest(
+            "https://www.scrapethissite.com/pages/advanced/?gotcha=csrf",
+            formdata={
+                "user": "test",
+                "pass": "test",
+                "csrf": csrf_token
+            },
+            callback=self.parse_login
+        )
+    
+    def parse_login(self, response):
+        print("[ Result ]:", response.css("div.row div::text").get())
+    # def parse(self, response):
+    #     table= response.css("table")
+
+    #     product_details = {}
+    #     for row in table.css("tr"):
+
+    #         heading = row.css("th::text").get()
+    #         data = row.css("td::text").get()
+
+    #         product_details[heading] = data
+
+    #     yield product_details
